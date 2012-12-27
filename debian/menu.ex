f516136661ac6045c46fc063cfb2bd003885465c
Devon Kearns <dookie@kali.org>
2012-12-27 07:39:14 -0700

    Commit before modifying

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(spooftooph):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="spooftooph" command="/usr/bin/spooftooph"
---
+++ debian/spooftooph.default.ex
@@ -0,0 +1,10 @@
+# Defaults for spooftooph initscript
+# sourced by /etc/init.d/spooftooph
+# installed at /etc/default/spooftooph by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
