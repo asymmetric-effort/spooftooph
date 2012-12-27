034634e833a9aec1576abfe642ff864cfd78e814
Devon Kearns <dookie@kali.org>
2012-12-27 07:46:59 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(spooftooph):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="spooftooph" command="/usr/bin/spooftooph"
--- debian/spooftooph.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the spooftooph package
-#
-0 4	* * *	root	[ -x /usr/bin/spooftooph_maintenance ] && /usr/bin/spooftooph_maintenance
